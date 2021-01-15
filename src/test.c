520399a5d6e194dc7bcba5a51f01816b1f217638
fakeshine <74741708+fakeshine@users.noreply.github.com>
2021-01-15 15:50:54 +0200

    Dupa teste

--- src/test.c
+++ src/test.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-void main()
+int main( int argc, char *argv[] )
 {
     int a,b;
-    printf("%d" , a+b);
+    if(argc != 2)
+    {
+            printf("numarul de argumente nu este corect");
+            return -1;
 
+    }
+
+    a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+    printf("Suma lui %d si %d este %d" , a , b , a+b);
+
+    return 0;
 }
